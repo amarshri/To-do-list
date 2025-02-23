ad3414bf66151c6dc25fb12e911a50890f56eb81
amarshri <amarshri6139@gmail.com>
2025-02-23 13:25:47 +0530

    adding js file

--- list.js
+++ list.js
@@ -1,7 +1,7 @@
 
 const inputbox = document.getElementById("input-box");
 const listbox = document.getElementById("list");
-document.getElementsByTagName("button");
+
 
 
 function addTask(){
@@ -9,10 +9,32 @@ function addTask(){
         alert("you must write something");
     }
     else{
-        let content = document.createElement("li");
-        content.innerHTML = inputbox.value;
-        listbox.appendChild(content);
+        let li= document.createElement("li");
+        li.innerHTML = inputbox.value;
+        listbox.appendChild(li);
+        let span=document.createElement("span");
+        span.innerHTML="\u00d7";
+        li.appendChild(span);
     }
     inputbox.value = "";
+    saveData();
 }
     
+listbox.addEventListener("click",function(e){
+    if(e.target.tagName ==="LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if (e.target.tagName === "SPAN"){
+        e.target.parentElement.remove()
+        saveData();
+    }
+},false);
+
+function saveData(){
+    localStorage.setItem("data",list.innerHTML);
+}
+function showTask(){
+    list.innerHTML = localStorage.getItem("data")
+}
+showTask();
